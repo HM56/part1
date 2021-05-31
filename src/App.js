f9448721978863df6400f6fee718cc48ee27fd7d
HM56 <harrymadan60@gmail.com>
2021-05-31 13:38:56 +0530

    destructuring

--- src/App.js
+++ src/App.js
@@ -1,51 +1,27 @@
 import React from 'react'
 
-const Header = (props) => {
-  return <h1>{props.course.name}</h1>
-}
+const Hello = ({name, age}) => {
 
-const Content = (props) => {
-  return(
-    <>
-      <Part part={props.course.parts[0]}/>
-      <Part part={props.course.parts[1]}/>
-      <Part part={props.course.parts[2]}/>
-    </>
-  )
-}
-
-const Total = (props) => {
-  return <p>Number of exercises {props.course.parts[0].exercises+props.course.parts[1].exercises+props.course.parts[2].exercises}</p>
-}
-
-const Part = (props) => {
-  return <p>{props.part.name} {props.part.exercises}</p>
-}
-
-const App = () => {
-  const course = {
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7
-      },
-      {
-        name: 'State of a component',
-        exercises: 14
-      }
-    ]
-  }
+  const bornYear = () => new Date().getFullYear() - age
 
   return (
     <div>
-      <Header course={course}/>
-      <Content course={course}/>
-      <Total course={course}/>
+      <p>Hello {name}, you are {age} years old</p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Kevin'
+  const age = 15
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Hardik" age="14"/>
+      <Hello name={name} age={age}/>
+      <Hello name="Aditya" age="14"/>
     </div>
   )
 }
