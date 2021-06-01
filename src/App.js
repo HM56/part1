7f241c480ea7689b65b1363b544a2a324468d0f5
HM56 <harrymadan60@gmail.com>
2021-06-01 13:31:52 +0530

    anecdotes completed

--- src/App.js
+++ src/App.js
@@ -6,44 +6,42 @@ const Button = ({handleClick, text}) => (
   </button>
 )
 
-const Statistic = ({text, value}) => {return <tr> <td>{text}</td> <td> {value}</td> </tr>}
-
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [bad, setBad] = useState(0)
-  const [neutral, setNeutral] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blod tests when dianosing patients'
+  ]
 
-  const handleGoodClick = () => setGood(good + 1)
-  const handleBadClick = () => setBad(bad + 1)
-  const handleNeutralClick = () => setNeutral(neutral + 1)
+  const [selected, setSelected] = useState(0)
+  const [votes, setVotes] = useState([0,0,0,0,0,0,0])
 
-  if(good+bad+neutral===0){
-    return(
-      <>
-        <h1>Give Feedback</h1>
-        <Button handleClick={handleGoodClick} text="good"/>
-        <Button handleClick={handleNeutralClick} text="neutral"/>
-        <Button handleClick={handleBadClick} text="bad"/>
-        <h1> Statistics </h1>
-        <p>No feedback given</p>
-      </>
-    )
+  const handleNextClick = () => {
+    console.log(anecdotes[selected], selected)
+    setSelected(Math.floor(Math.random()*7))
   }
+  const handleVoteClick = () => {
+    const copy = [...votes]
+    copy[selected]+=1
+    // setVotes([...votes, votes[selected]=votes[selected]+1])
+    // console.log(votes[selected])
+    setVotes(copy)
+  }
+
+  const max = (votes.indexOf((Math.max(...votes))))
 
   return (
     <div>
-      <h1>Give Feedback</h1>
-      <Button handleClick={handleGoodClick} text="good"/>
-      <Button handleClick={handleNeutralClick} text="neutral"/>
-      <Button handleClick={handleBadClick} text="bad"/>
-      <h1> Statistics </h1>
-      <table>
-        <Statistic text="good" value={good}/>
-        <Statistic text="neutral" value={neutral}/>
-        <Statistic text="bad" value={bad}/>
-        <Statistic text="average" value={(good-bad)/(good+bad+neutral)}/>
-        <Statistic text="positive" value={good/(good+neutral+bad)*100}/>
-      </table>
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]} <br/> has {votes[selected]} votes</p>
+      <Button handleClick={handleVoteClick} text="Vote"/>
+      <Button handleClick={handleNextClick} text="Next"/>
+      <h1>Anecdote with most votes</h1>
+      <p>{anecdotes[max]} <br/> has {votes[max]} votes</p>
     </div>
   )
 }
